c2990622a200e0dd19ae5e92d1fc7214881d2f45
3nt3 <gott@3nt3.de>
2023-04-25 11:35:06 +0200

    Actually fetch Parcels from database

--- src/routes/home.rs
+++ src/routes/home.rs
@@ -1,10 +1,13 @@
 use cfg_if::cfg_if;
 use leptos::*;
 use leptos_router::*;
+use serde::{Deserialize, Serialize};
+
+use crate::components::container::*;
 
 cfg_if! {
   if #[cfg(feature = "ssr")] {
-    use sqlx::{Connection, PgConnection};
+    use sqlx::{Connection, PgConnection, postgres::PgRow};
 
     pub async fn db() -> Result<PgConnection, ServerFnError> {
       PgConnection::connect("postgres://parcels:parcels@localhost:46257/parcels").await.map_err(|e| ServerFnError::ServerError(e.to_string()))
@@ -15,28 +18,109 @@ cfg_if! {
         _ = DeleteParcel::register();
         _ = GetParcels::register();
     }
-  }
+
+    #[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize, sqlx::FromRow)]
+    pub struct Parcel {
+      pub id: i32,
+      pub tracking_id: String,
+      pub created_at: chrono::DateTime<chrono::Utc>,
+      pub carrier: Carrier
+    }
+
+
+    impl sqlx::Decode<'_, sqlx::Postgres> for Carrier {
+      fn decode(
+        value: sqlx::postgres::PgValueRef<'_>,
+      ) -> Result<Self, Box<dyn std::error::Error + 'static + Send + Sync>> {
+        let carrier = String::decode(value)?;
+        Ok(match carrier.as_str() {
+          "dhl" => Carrier::DHL,
+          _ => Carrier::Unknown(carrier),
+        })
+      }
+    }
+
+    impl sqlx::Type<sqlx::Postgres> for Carrier {
+      fn type_info() -> sqlx::postgres::PgTypeInfo {
+        sqlx::postgres::PgTypeInfo::with_name("varchar")
+      }
+    }
+
+  } else {
+    #[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
+    pub struct Parcel {
+      pub id: i32,
+      pub tracking_id: String,
+      pub created_at: chrono::DateTime<chrono::Utc>,
+      pub carrier: Carrier
+    }
+}
+}
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub enum Carrier {
+    DHL,
+    Unknown(String),
+}
+
+impl std::fmt::Display for Carrier {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Carrier::DHL => write!(f, "dhl"),
+            Carrier::Unknown(carrier) => write!(f, "{}", carrier),
+        }
+    }
+}
+
+impl Serialize for Carrier {
+    fn serialize<S: serde::Serializer>(&self, serializer: S) -> Result<S::Ok, S::Error> {
+        serializer.serialize_str(match self {
+            Carrier::DHL => "dhl",
+            Carrier::Unknown(carrier) => carrier,
+        })
+    }
+}
+
+impl<'de> Deserialize<'de> for Carrier {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: serde::Deserializer<'de>,
+    {
+        let s = String::deserialize(deserializer)?;
+        Ok(match s.as_str() {
+            "dhl" => Carrier::DHL,
+            _ => Carrier::Unknown(s),
+        })
+    }
 }
 
 #[server(AddParcel, "/api")]
 pub async fn add_parcel(parcel_id: String) -> Result<String, ServerFnError> {
     let mut conn = db().await?;
 
-    let lol = sqlx::query!("insert into parcels (tracking_id) values ($1) returning id", parcel_id)
-      .fetch_one(&mut conn)
-      .await
-      .map_err(|e| ServerFnError::ServerError(e.to_string()))?
-      .id;
-    dbg!(&lol);
-
+    let lol = sqlx::query!(
+        "insert into parcels (tracking_id) values ($1) returning id",
+        parcel_id
+    )
+    .fetch_one(&mut conn)
+    .await
+    .map_err(|e| ServerFnError::ServerError(e.to_string()))?
+    .id;
 
     println!("Added parcel {} ({})", parcel_id, &lol);
     Ok(format!("Added parcel {} ({})", parcel_id, &lol))
 }
 
 #[server(GetParcels, "/api")]
-async fn get_parcels(cx: Scope) -> Result<Vec<String>, ServerFnError> {
-    Ok(vec!["parcel1".to_string(), "parcel2".to_string()])
+async fn get_parcels(cx: Scope) -> Result<Vec<Parcel>, ServerFnError> {
+    let mut conn = db().await?;
+
+    let parcels = sqlx::query_as::<_, Parcel>("select * from parcels")
+        .fetch_all(&mut conn)
+        .await
+        .map_err(|e| ServerFnError::ServerError(e.to_string()))?;
+
+    Ok(parcels)
 }
 
 #[server(DeleteParcel, "/api")]
@@ -56,34 +140,38 @@ pub fn HomePage(cx: Scope) -> impl IntoView {
     );
 
     view! {cx,
-      <h1 class="text-red-300">"Home"</h1>
-      <MultiActionForm action=add_parcel>
-        <label>
-          "Add parcel"
-          <input type="text" name="parcel_id" />
-        </label>
-        <input type="submit" value="Submit"/>
-      </MultiActionForm>
-      <Transition fallback=move || view! {cx, <div>"Loading..."</div>} >
-        {move || {
-          parcels.read(cx).map(move |parcels| match parcels {
-            Err(why) => { view! { cx, <div>{format!("Error: {why}")}</div> }.into_view(cx) },
-            Ok(parcels) => {
-              parcels.into_iter().map(move |parcel| {
-                view! {cx,
-                  <div>
-                    <span>{parcel.clone()}</span>
-                    <ActionForm action=delete_parcel>
-                      <input type="hidden" name="parcel_id" value={parcel.clone()} />
-                      <input type="submit" value="Delete"/>
-                    </ActionForm>
-                  </div>
-                }
-              }).collect::<Vec<_>>()
-              .into_view(cx)
-            }
-          }).unwrap_or_default()
-        }}
-      </Transition>
+      <div class="max-w-screen-sm flex flex-col gap-8 items-start">
+        <h1 class="mt-8 text-slate-50 text-4xl font-bold">"Home"</h1>
+        <Container>
+          <MultiActionForm action=add_parcel class="flex flex-col gap-4 items-start">
+            <label class="text-2xl flex flex-col items-start">
+              "Add parcel"
+              <input type="text" name="parcel_id" class="bg-slate-700 shadow-slate-900" />
+            </label>
+            <input type="submit" value="Submit"/>
+          </MultiActionForm>
+        </Container>
+        <Transition fallback=move || view! {cx, <div>"Loading..."</div>} >
+          {move || {
+            parcels.read(cx).map(move |parcels| match parcels {
+              Err(why) => { view! { cx, <div>{format!("Error: {why}")}</div> }.into_view(cx) },
+              Ok(parcels) => {
+                parcels.into_iter().map(move |parcel: Parcel| {
+                  view! {cx,
+                    <div>
+                      <div>{format!("{:?}", &parcel)}</div>
+                      <ActionForm action=delete_parcel>
+                        <input type="hidden" name="parcel_id" value={&parcel.tracking_id.clone()} />
+                        <input type="submit" value="Delete"/>
+                      </ActionForm>
+                    </div>
+                  }
+                }).collect::<Vec<_>>()
+                .into_view(cx)
+              }
+            }).unwrap_or_default()
+          }}
+        </Transition>
+      </div>
     }
 }
